61ef192ae1dfa3999d300b1b29f9d05ed1831173
watalo <watalo@163.com>
2021-02-26 16:40:53 +0800

    备注

---
+++ weekreport-robot/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@NAME		:main.py
+@TIME		:2021/02/25 00:45:09
+@AUTHOR     :watalo
+@VERSION	:0.0.x
+'''
+
+'''
+制作微信机器人，用于统计每周的工作计划
+1、工作群里增加一个机器人
+2、机器人的功能：统计周报
+'''
+
+
+
+
+class Weekreport:
+    def __init__(self):
+        pass
+
+def Func():
+    pass
+
+
+if __name__ == '__main__':
