c23d2530abc69bc1eb19746733468f3b80bb173d
watalo <watalo@163.com>
2021-03-02 00:49:56 +0800

    add prod module

--- project-pool/scr/prod.py
+++ project-pool/scr/prod.py
@@ -6,26 +6,32 @@
 @AUTHOR     :watalo
 @VERSION	:0.0.x
 '''
+'''
+产品类，封装各种产品的属性
+    -贷款
+    -银票
+    -保函
+    ...
+'''
 
 class Loans(object):
     '''
     表内贷款
     '''
     
-    def __init__(self, amount, start_time, end_time, interest_rate, info):
+    def __init__(self, amount, start_time, end_time, revenue_rate, info):
         '''
         :description: 
         :param {amount}: 字面，如下
         :param {start_time}:
         :param {end_time}:
-        :param {interest_rate}:
+        :param {revenue_rate}:
         :param {info}:
         :return:
         '''
         self.amount = amount
         self.start_time = start_time
-        self.end_time = end_time
-        self.interest_rate = interest_rate
+        self.end_time = end_revenue_rate = revenue_rate
         self.info = info
 
     def length(self):
@@ -34,11 +40,11 @@ class Loans(object):
     def float_rate(self):
         base_rate_dict = {1:0.0365, 3:0.0385, 5:0.0435}
         if self.length() <= 365:
-            return 100*((self.interest_rate/base_rate_dict[1]) - 1)
+            return 100*((self.revenue_rate/base_rate_dict[1]) - 1)
         if self.length() >= 365 and self.length() <= 365*3:
-            return 100*((self.interest_rate/base_rate_dict[3]) - 1)
+            return 100*((self.revenue_rate/base_rate_dict[3]) - 1)
         if self.length() >= 365*5:
-            return 100*((self.interest_rate/base_rate_dict[5]) - 1)
+            return 100*((self.revenue_rate/base_rate_dict[5]) - 1)
         else:
             pass
     
@@ -49,28 +55,45 @@ class Notes(object):
     '''
     银行承兑汇票
     '''
-    def __init__(self, var):
+    def __init__(self, amount, start_time, end_time, revenue_rate, bargain):
         '''
-        :description:
-        :param {var}:
+        :description: 
+        :param {amount}: 字面，如下
+        :param {start_time}:
+        :param {end_time}:
+        :param {revenue_rate}:
+        :param {bargaain}:
         :return:
         '''
-        self.var = var
-        pass
+        self.amount = amount
+        self.start_time = start_time
+        self.end_time = end_time
+        self.revenue_rate = revenue_rate
+        self.bargain = bargain
+
+    def bargain_rate(self):
+        return self.bargin / self.amount
 
 class Letters(object):
     '''
-    :description:
+    保函
     '''
-    def __init__(self, var):
+    def __init__(self, amount, start_time, end_time, revenue_rate, bargain):
         '''
-        :description:
-        :param {var}:
+        :description: 
+        :param {amount}: 字面，如下
+        :param {start_time}:
+        :param {end_time}:
+        :param {revenue_rate}:
+        :param {bargaain}:
         :return:
         '''
-        self.var = var
-        pass
+        self.amount = amount
+        self.start_time = start_time
+        self.end_time = end_time
+        self.revenue_rate = revenue_rate
+        self.bargain = bargain
 
+    def bargain_rate(self):
+        return self.bargin / self.amount
 
-if __name__ == '__main__':
-    pass
